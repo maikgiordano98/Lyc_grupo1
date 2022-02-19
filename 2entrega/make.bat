b9a168fef3a2432a603fc8af4e32766168b11015
Michael Giordano <70980802+maikgiordano98@users.noreply.github.com>
2022-02-19 13:41:27 -0300

    makes

--- 2entrega/make.bat
+++ 2entrega/make.bat
@@ -1,4 +1,4 @@
 C:\GnuWin32\bin\flex.exe Lexico.l
 C:\GnuWin32\bin\bison.exe -dyv Sintactico.y
-C:\MinGW\bin\gcc.exe -o parser.exe -w lex.yy.c y.tab.c symbol_table.c
+C:\MinGW\bin\gcc.exe -o parser.exe -w lex.yy.c y.tab.c symbol_table.c Pila.h Cola.h Lista.h funciones.h
 
